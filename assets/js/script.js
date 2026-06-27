61eb66cccef2f49c81bda1b425c0e1f0f79c5f9f
RxN <120871604+Ricofzm@users.noreply.github.com>
2026-06-27 15:22:19 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
 let selectedProduct = null;
+
+const cards = document.querySelectorAll(".voucher-card");
+
+const prices = [
+    12850,
+    65921,
+    131850,
+    259900
+];
+
+cards.forEach((card, index) => {
+
+    card.addEventListener("click", () => {
+
+        cards.forEach(c => c.classList.remove("active"));
+
+        card.classList.add("active");
+
+        document.getElementById("checkout-price").innerHTML =
+            "Rp " + prices[index].toLocaleString("id-ID");
+
+        document.getElementById("checkout-name").innerHTML =
+            card.querySelector("h3").innerHTML;
+
+    });
+
+});
