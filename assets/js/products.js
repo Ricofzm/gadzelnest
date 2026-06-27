fc5433a45b0484023c21bf9a0a670afd99662d38
RxN <120871604+Ricofzm@users.noreply.github.com>
2026-06-27 15:12:19 +0800

    Create products.js

---
+++ assets/js/products.js
@@ -0,0 +1,26 @@
+const products = [
+    {
+        id:1,
+        name:"626 Cash Pack",
+        bonus:"600 + 26 Bonus",
+        price:12850
+    },
+    {
+        id:2,
+        name:"3130 Cash Pack",
+        bonus:"3000 + 130 Bonus",
+        price:65921
+    },
+    {
+        id:3,
+        name:"6366 Cash Pack",
+        bonus:"6000 + 366 Bonus",
+        price:131850
+    },
+    {
+        id:4,
+        name:"12800 Cash Pack",
+        bonus:"12000 + 800 Bonus",
+        price:259900
+    }
+];
