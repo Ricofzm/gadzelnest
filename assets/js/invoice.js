cdda42ded349a6e59ff6b4a2c97119e3cfbdbab2
RxN <120871604+Ricofzm@users.noreply.github.com>
2026-06-28 22:50:58 +0800

    Update invoice.js

--- assets/js/invoice.js
+++ assets/js/invoice.js
@@ -2,21 +2,14 @@ const order = JSON.parse(localStorage.getItem("order"));
 
 if(!order){
     window.location.href="../index.html";
-
-// Isi data invoice
-if (order) {
-
-    document.getElementById("invoice-char").textContent = order.charId;
-
-    document.getElementById("invoice-server").textContent = order.serverId;
-
-    document.getElementById("invoice-voucher").textContent = order.voucher;
-
-    document.getElementById("invoice-payment").textContent = order.payment;
-
-    document.getElementById("invoice-total").textContent = order.total;
-
 }
+
+document.getElementById("invoice-order").textContent = order.orderId;
+document.getElementById("invoice-char").textContent = order.charId;
+document.getElementById("invoice-server").textContent = order.serverId;
+document.getElementById("invoice-voucher").textContent = order.voucher;
+document.getElementById("invoice-payment").textContent = order.payment;
+document.getElementById("invoice-total").textContent = order.total;
 
 // Countdown
 let time = 15 * 60;
