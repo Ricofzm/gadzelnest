ea9b86a2448ccf9020e486fb2f408c835f299bcd
RxN <120871604+Ricofzm@users.noreply.github.com>
2026-06-28 21:06:27 +0800

    Create invoice.js

---
+++ assets/js/invoice.js
@@ -0,0 +1,15 @@
+const order = JSON.parse(localStorage.getItem("order"));
+
+if(order){
+
+document.getElementById("invoice-char").textContent = order.charId;
+
+document.getElementById("invoice-server").textContent = order.serverId;
+
+document.getElementById("invoice-voucher").textContent = order.voucher;
+
+document.getElementById("invoice-payment").textContent = order.payment;
+
+document.getElementById("invoice-total").textContent = order.total;
+
+}
