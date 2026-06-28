0c84c52e67fcdcc85b267baf70a3c653f58104c8
RxN <120871604+Ricofzm@users.noreply.github.com>
2026-06-28 22:07:41 +0800

    Update invoice.js

--- assets/js/invoice.js
+++ assets/js/invoice.js
@@ -1,17 +1,55 @@
 const order = JSON.parse(localStorage.getItem("order"));
-const orderId = "GN-" + Date.now();
 
-document(.getElementById("invoice-order").textContent = orderId;
+document.getElementById("invoice-order").textContent = orderId;
 
-if(order){
+// Isi data invoice
+if (order) {
 
     document.getElementById("invoice-char").textContent = order.charId;
-    
+
     document.getElementById("invoice-server").textContent = order.serverId;
-    
+
     document.getElementById("invoice-voucher").textContent = order.voucher;
-    
+
     document.getElementById("invoice-payment").textContent = order.payment;
 
     document.getElementById("invoice-total").textContent = order.total;
+
 }
+
+// Countdown
+let time = 15 * 60;
+
+const timer = setInterval(() => {
+
+    const minute = Math.floor(time / 60);
+
+    const second = time % 60;
+
+    document.getElementById("countdown").textContent =
+        `${minute}:${second.toString().padStart(2, "0")}`;
+
+    if (time <= 0) {
+
+        clearInterval(timer);
+
+        document.querySelector(".payment-status").textContent =
+            "❌ Invoice Kedaluwarsa";
+
+    }
+
+    time--;
+
+}, 1000);
+
+// Copy Order ID
+document.getElementById("copyOrder").addEventListener("click", async () => {
+
+    const orderId =
+        document.getElementById("invoice-order").textContent;
+
+    await navigator.clipboard.writeText(orderId);
+
+    alert("Order ID berhasil disalin.");
+
+});
