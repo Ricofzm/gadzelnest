f845e342ee20c90c174bab31ad87ce07f0e39d0f
RxN <120871604+Ricofzm@users.noreply.github.com>
2026-06-28 21:37:53 +0800

    Update invoice.js

--- assets/js/invoice.js
+++ assets/js/invoice.js
@@ -1,15 +1,19 @@
 const order = JSON.parse(localStorage.getItem("order"));
 
 if(order){
+    
+    const orderId = "GN-" + Date.now();
 
-document.getElementById("invoice-char").textContent = order.charId;
+    document(.getElementById("invoice-order").textContent = orderId;
 
-document.getElementById("invoice-server").textContent = order.serverId;
-
-document.getElementById("invoice-voucher").textContent = order.voucher;
-
-document.getElementById("invoice-payment").textContent = order.payment;
-
-document.getElementById("invoice-total").textContent = order.total;
+    document.getElementById("invoice-char").textContent = order.charId;
+    
+    document.getElementById("invoice-server").textContent = order.serverId;
+    
+    document.getElementById("invoice-voucher").textContent = order.voucher;
+    
+    document.getElementById("invoice-payment").textContent = order.payment;
+    
+    document.getElementById("invoice-total").textContent = order.total;
 
 }
