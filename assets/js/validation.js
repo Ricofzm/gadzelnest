2c2f2f123381aca5022f8f4e40fec2fb51f2ff4f
RxN <120871604+Ricofzm@users.noreply.github.com>
2026-06-29 20:55:56 +0800

    Update validation.js

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,1 +1,25 @@
+function validateCheckout(){
 
+    const charInput = document.getElementById("charId");
+    const serverInput = document.getElementById("serverId");
+    const checkoutBtn = document.getElementById("checkoutBtn");
+
+    const charId = charInput.value.trim();
+    const serverId = serverInput.value.trim();
+
+    const voucher =
+        document.getElementById("summary-voucher").textContent;
+
+    const payment =
+        document.getElementById("summary-payment").textContent;
+
+    checkoutBtn.disabled = !(
+        charId &&
+        serverId &&
+        voucher !== "-" &&
+        payment !== "-"
+    );
+
+}
+
+window.validateCheckout = validateCheckout;
