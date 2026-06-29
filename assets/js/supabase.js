93419e90bd86960b01d1be40ed610311d59e1e31
RxN <120871604+Ricofzm@users.noreply.github.com>
2026-06-29 14:43:53 +0800

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,5 +1,4 @@
 const SUPABASE_URL = "https://eqyedayduqhkaqakclsa.supabase.co";
-
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxeWVkYXlkdXFoa2FxYWtjbHNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2NTYzNDgsImV4cCI6MjA5ODIzMjM0OH0.sRZJuTffjt4uiSr8PjrrGnNOun5Wy18inhQ4P1poTYA";
 
 const supabaseClient = window.supabase.createClient(
