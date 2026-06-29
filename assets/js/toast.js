70c3f06d850bcbccaa9db1ff5ed0dcb001b764a4
RxN <120871604+Ricofzm@users.noreply.github.com>
2026-06-29 20:41:35 +0800

    Update toast.js

--- assets/js/toast.js
+++ assets/js/toast.js
@@ -1,1 +1,23 @@
+let toastTimeout;
 
+function showToast(message){
+
+    const toast=document.getElementById("toast");
+
+    const toastMessage=document.getElementById("toast-message");
+
+    clearTimeout(toastTimeout);
+
+    toastMessage.textContent=message;
+
+    toast.classList.add("show");
+
+    toastTimeout=setTimeout(()=>{
+
+        toast.classList.remove("show");
+
+    },2500);
+
+}
+
+window.showToast = showToast;
