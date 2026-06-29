9c9717f2e8dda1a70a2f0414d36f3f3c1458c783
RxN <120871604+Ricofzm@users.noreply.github.com>
2026-06-29 20:42:44 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -179,25 +179,3 @@ function scrollToTopup(){
     });
 
 }
-
-let toastTimeout;
-
-function showToast(message){
-
-    const toast=document.getElementById("toast");
-
-    const toastMessage=document.getElementById("toast-message");
-
-    clearTimeout(toastTimeout);
-
-    toastMessage.textContent=message;
-
-    toast.classList.add("show");
-
-    toastTimeout=setTimeout(()=>{
-
-        toast.classList.remove("show");
-
-    },2500);
-
-}
