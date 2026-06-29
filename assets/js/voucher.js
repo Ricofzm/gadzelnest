c86040e81a1e6f05a1c70fd796234ac7ad81529b
RxN <120871604+Ricofzm@users.noreply.github.com>
2026-06-29 21:14:23 +0800

    Update voucher.js

--- assets/js/voucher.js
+++ assets/js/voucher.js
@@ -1,1 +1,27 @@
+function activateVoucher() {
 
+    const cards = document.querySelectorAll(".voucher-card");
+    const checkoutName = document.getElementById("checkout-name");
+    const checkoutPrice = document.getElementById("checkout-price");
+
+    cards.forEach(card => {
+        card.addEventListener("click",()=>{
+            cards.forEach(c => c.classList.remove("active"));
+            card.classList.add("active");
+
+            checkoutName.textContent = card.dataset.name;
+            checkoutPrice.textContent =
+                "Rp " + Number(card.dataset.price).toLocaleString("id-ID");
+
+            // Update Ringkasan Pesanan
+            document.getElementById("summary-voucher").textContent = card.dataset.name;
+            document.getElementById("summary-total").textContent =
+                "Rp " + Number(card.dataset.price).toLocaleString("id-ID");
+                
+            validateCheckout();
+            showToast("✅ Voucher berhasil dipilih");
+        });
+    });
+}
+
+window.activateVoucher = activateVoucher;
