0dec76b2bdeb755e92621f1db02b2f5d2dbde4f7
RxN <120871604+Ricofzm@users.noreply.github.com>
2026-07-01 12:34:53 +0800

    Update utils.js

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,1 +1,19 @@
+// ==========================
+// GENERATE ORDER ID
+// ==========================
 
+function generateOrderId() {
+
+    const now = new Date();
+
+    return `GN-${
+        now.getFullYear()
+    }${
+        String(now.getMonth() + 1).padStart(2, "0")
+    }${
+        String(now.getDate()).padStart(2, "0")
+    }-${
+        Date.now().toString().slice(-6)
+    }`;
+
+}
