c0911386be8103140890fcf2b50438bcda88158b
RxN <120871604+Ricofzm@users.noreply.github.com>
2026-07-09 14:40:18 +0800

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,38 +2,43 @@ checkSession();
 
 async function checkSession(){
 
-    const {
-        data:{session}
-    }=await supabaseClient.auth.getSession();
-
-    if(session){
-
+    const form=document.getElementById("loginForm");
+    const msg=document.getElementById("msg");
+    
+    form.addEventListener("submit",async(e)=>{
+    
+        e.preventDefault();
+    
+        const email=document.getElementById("email").value;
+        const password=document.getElementById("password").value;
+    
+        const res=await fetch(
+            "https://gnest-api.enrikofzm.workers.dev/login",
+            {
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body:JSON.stringify({
+                    email,
+                    password
+                })
+            }
+        );
+    
+        const result=await res.json();
+    
+        if(!result.success){
+    
+            msg.textContent=result.message;
+            return;
+    
+        }
+    
+        localStorage.setItem("admin_token",result.token);
+    
         location.href="/admin/admin.html";
-
-    }
-
-}
-
-const form = document.getElementById("loginForm");
-const msg = document.getElementById("msg");
-
-form.addEventListener("submit", async (e)=>{
-
-    e.preventDefault();
-
-    const email=document.getElementById("email").value;
-    const password=document.getElementById("password").value;
-
-    const {error}=await supabaseClient.auth.signInWithPassword({
-        email,
-        password
+    
     });
-
-    if(error){
-        msg.textContent=error.message;
-        return;
-    }
-
-    location.href="/admin/admin.html";
 
 });
